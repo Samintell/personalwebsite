eb1ca84860553581729192101762d8232051159d
Samintell <joshmatthew22@hotmail.com>
2020-10-02 23:37:32 -0500

    started kbdyndemo

---
+++ kbdyndemo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class kbdyndemoConfig(AppConfig):
+    name = 'kbdyndemo'
---
+++ kbdyndemo/urls.py
@@ -0,0 +1,16 @@
+"""
+Definition of urls for personalwebsite.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+#from app import forms, views
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register')
+]
