98714c055dac0551bdcf1a59eda665ef36064cf2
Samintell <joshmatthew22@hotmail.com>
2020-10-04 22:55:46 -0500

    ready for first deployment test

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9232fb62-1154-4953-9471-4293f9864155'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9232fb62-1154-4953-9471-4293f9864155')
+#SECRET_KEY = '9232fb62-1154-4953-9471-4293f9864155'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
