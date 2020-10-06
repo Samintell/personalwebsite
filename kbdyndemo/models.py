d5b61e0fc7664933464cbf9ee6dc47866c00e5dd
Samintell <joshmatthew22@hotmail.com>
2020-10-06 01:11:00 -0500

    model of user input

--- kbdyndemo/models.py
+++ kbdyndemo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserInput(models.Model):
+    userID = models.CharField(max_length=40)
+    text = models.TextField()
+    typingData = models.TextField()
+    dateOfData = models.DateTimeField()
+
+    def __str__(self):
+        return self.userID + ":" + self.dateOfData + ':' + self.typingData
--- kbdyndemo/views.py
+++ kbdyndemo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import UserInput
+from django.utils import timezone
 
 # Create your views here.
 def register(request):
@@ -12,3 +14,20 @@ def register(request):
             
         }
     )
+
+def regsubmit(request):
+    try:
+        plaintext = request.POST['plaintext']
+        hiddenData = request.POST['hiddenData']
+        user = request.POST['userID']
+    except (KeyError):
+        return render(
+        request,
+        'kbdyndemo/input.html',
+            {
+            
+            }
+        )
+    else:
+        u = UserInput(userID=user, text=plaintext, typingData=hiddenData, dateOfData=timezone.now())
+        u.save()
